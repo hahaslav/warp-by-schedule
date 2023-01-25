d2eb55bee5282997ab6ff4b424b96479541720f7
hahaslav <yara-2004@ukr.net>
2023-01-25 11:48:43 +0200

    Add item chooser

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import json
 import time
 
+TIME_FORMAT = "%H:%M"
 FIELD_UNTIL = "until"
+FIELD_NAME = "name"
+FIELD_URL = "url"
 
 
 def load_file():
@@ -26,7 +29,7 @@ def parse_time(schedule):
     Converts human time into time.struct_time for easy sorting
     """
     for el in schedule:
-        el[FIELD_UNTIL] = time.strptime(el[FIELD_UNTIL], "%H:%M")
+        el[FIELD_UNTIL] = time.strptime(el[FIELD_UNTIL], TIME_FORMAT)
 
 
 def sort_by_time(schedule):
@@ -43,12 +46,35 @@ def sort_by_time(schedule):
                 changed = True
 
 
+def get_current_time():
+    """
+    Returns current time, ignoring the current day and year
+    """
+    return time.strptime(time.strftime(TIME_FORMAT, time.localtime()), TIME_FORMAT)
+
+
+def find_nearest_item(schedule, target_time: time.struct_time):
+    """
+    Returns the nearest item to the given time
+    """
+    result = None
+
+    for el in schedule:
+        if target_time < el[FIELD_UNTIL]:
+            result = el
+
+    return result
+
+
 def main():
     start_date, schedule = load_file()
     day_of_week = get_today_day_of_week()
     today_schedule = schedule[day_of_week]
     parse_time(today_schedule)
     sort_by_time(today_schedule)
+
+    nearest_item = find_nearest_item(today_schedule, get_current_time())
+    print(nearest_item[FIELD_NAME])
 
 
 if __name__ == '__main__':
